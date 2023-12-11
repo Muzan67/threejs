1dc83d94e79348db69b67385d4c778e68d40bbfd
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2023-12-11 17:24:35 -0500

    updated pages section

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapShot } from "valtio";
+
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation } from "../config/motion";
 
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
-}
+  return <div>Customizer</div>;
+};
 
-export default Customizer
+export default Customizer;
